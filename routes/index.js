bed1cf2f60af5d7e05dc9b176ce3a93fbb251e5d
Anders Wiberg Olsen <anders@wiberg.tech>
2022-05-28 22:41:43 +0200

    Add UI and login for login/logout

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-let defaultName = 'World';
-
 router.get('/', (req, res) => {
-    // const user = req.oidc.user;
     const isAuthenticated = req.oidc.isAuthenticated();
-    let { name } = req.query;
-    if (!name) {
-        name = defaultName;
+
+    if(!!req.oidc.user) {
+        console.log(JSON.stringify(req.oidc.user));
     }
+
     res.render('index', {
         title: 'My Express App',
-        name,
         isAuthenticated,
+        user: req.oidc.user
     });
 });
 
