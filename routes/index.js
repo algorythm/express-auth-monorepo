7b0bbdca89b555c64e0bd3d5931f00a97cce8952
Anders Wiberg Olsen <anders@wiberg.tech>
2022-05-28 21:45:06 +0200

    Add OIDC boilerplate

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,17 @@ const router = express.Router();
 let defaultName = 'World';
 
 router.get('/', (req, res) => {
+    // const user = req.oidc.user;
+    const isAuthenticated = req.oidc.isAuthenticated();
     let { name } = req.query;
     if (!name) {
         name = defaultName;
     }
-    res.render('index', { title: 'My Express App', name });
+    res.render('index', {
+        title: 'My Express App',
+        name,
+        isAuthenticated,
+    });
 });
 
 module.exports = router;
